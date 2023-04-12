c20892823080e087ba9666a63ed22aef2f046b8f
projectwork1029 <130593998+projectwork1029@users.noreply.github.com>
2023-04-12 23:09:24 +0530

    final commit

---
+++ app_rest.js
@@ -0,0 +1,19 @@
+var big_wrapper;
+var hamburger_menu;
+
+function declare() {
+  big_wrapper = document.querySelector(".big-wrapper");
+  hamburger_menu = document.querySelector(".hamburger-menu");
+}
+
+const main = document.querySelector("main");
+
+declare();
+
+function events() {
+  hamburger_menu.addEventListener("click", () => {
+    big_wrapper.classList.toggle("active");
+  });
+}
+
+events();
